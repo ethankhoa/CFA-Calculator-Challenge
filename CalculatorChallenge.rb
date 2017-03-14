040c3535ec216a22d369af6c4afdbd6b52c63354
ethankhoa <ethan.huynh@gmail.com>
2017-03-14 15:20:11 +1100

    first commit

---
+++ CalculatorChallenge.rb
@@ -0,0 +1,64 @@
+#Calculator Challenge
+#
+#Create a calculator application
+#
+#Your calculator should:
+#
+#1. ask the user for two numbers
+#2. ask the user whether they want o add, subtract or multiply return the result
+#
+#Bonus:
+#
+#4. If the answer is 42, tell the user the meaning of life.
+
+# Addition method
+def add(n1,n2)
+  answer = n1 + n2
+  display(answer)
+end
+
+# Subtraction method
+def subtract(n1, n2)
+  answer = n1 - n2
+  display(answer)
+end
+
+# Multiply method
+def multiply(n1, n2)
+  answer = n1 * n2
+  display(answer)
+end
+
+# Display result
+def display(result)
+  puts "Your answer is #{result}."
+  if result == 42
+    puts "You have discovered the answer to the Ultimate Question of Life, the Universe, and Everything. Have a nice day."
+  else
+end
+end
+
+
+puts "Hi I'm a calculator, please give me 2 numbers."
+
+number1 = gets.chomp.to_i
+number2 = gets.chomp.to_i
+
+puts "Your numbers are #{number1} and #{number2}."
+
+puts "Would you like to add, subtract, or multiply these?"
+
+selection = gets.chomp.downcase # downcase changes input to all lowercase
+
+
+if selection == "add"
+# add the numbers
+  puts add(number1, number2)
+  puts "Answer: #{number1 + number2}"
+elsif selection == "subtract"
+  puts subtract(number1, number2)
+  # subtract
+elsif selection == "multiply"
+  puts multiply(number1, number2)
+  # multiply
+end
